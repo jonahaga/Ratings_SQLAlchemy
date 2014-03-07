e091265d772a761dd901c1b6b2294a46b493a993
Hackbright Student <info@hackbrightacademy.com>
2014-03-06 18:07:07 -0800

    Working importing data for movie ratings

--- model.py
+++ model.py
@@ -1,13 +1,54 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+#this is nonstandard use of python class attributes... this is for SQLAlchemy only
+# if nullable = true it's optional, it can be left blank
+#the datatypes in the following are for SQL Alchemy only
+class User(Base):
+    __tablename__= "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    gender = Column(String(8), nullable = True)
+    zipcode = Column (String(15), nullable = True)
+
+class Movies(Base):
+    __tablename__= "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64), nullable = False)
+    released_at = Column(DateTime(timezone = False), nullable = True)
+    imdb_url = Column(String(64))
+
+class Ratings(Base):
+    __tablename__= "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer, nullable = False)
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,22 +1,62 @@
 import model
 import csv
+import datetime
+
+# open a file
+# read a line
+# parse a line
+# create an object
+# add the object to a session
+# commit
+# repeat until done
 
 def load_users(session):
     # use u.user
-    pass
+    # id|age|gender|occupation|zipcode
+    # 1|24|M|technician|85711
+    with open('seed_data/u.user', 'rb') as f:
+        reader = csv.reader(f, delimiter='|')
+        for row in reader:
+            new_user = model.User(age=int(row[1]), gender=row[2], zipcode=row[4])
+
+            session.add(new_user)
+        
+    session.commit()
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as f:
+        reader = csv.reader(f, delimiter = "|")
+        for row in reader:
+            date_string = row[2]
+            if date_string != "":
+                pattern = "%d-%b-%Y"
+                formatted_date = datetime.datetime.strptime(date_string, pattern)
+
+            new_movie = model.Movies(name=row[1].decode("latin-1"), released_at=formatted_date, imdb_url=row[3])
+
+            session.add(new_movie)
+
+    session.commit()
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as f:
+        reader = csv.reader(f, delimiter = " ")
+        for row in reader:
+            new_rating = model.Ratings(movie_id=row[0], user_id=row[1], rating=row[2])
+
+            session.add(new_rating)
+
+    session.commit()
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    # load_users(session)
+    load_movies(session)
+    load_ratings(session)
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
